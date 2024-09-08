746834c1c04df6ed30611631dd4672e307b7d48c
shivendra <shivendrakeshari581@gmail.com>
2024-09-08 18:27:45 +0530

    initialize

---
+++ src/Firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAXKPZWWDlOWhSqsE_oG9sfpE7HS5aw4vg",
+
+  authDomain: "fir-858d6.firebaseapp.com",
+
+  projectId: "fir-858d6",
+
+  storageBucket: "fir-858d6.appspot.com",
+
+  messagingSenderId: "926554219354",
+
+  appId: "1:926554219354:web:8fed834dd5f172908a8963",
+
+  measurementId: "G-W00C9Q7X09"
+
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
